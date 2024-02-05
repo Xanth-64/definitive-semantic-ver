0c6d327231e42fc983bfc6e308f1c716cb65cf96
Xanth-64 <bet.and.xanth@gmail.com>
2024-02-05 16:05:11 -0400

    fix: Bad sustitions fix

--- .github/workflows/scripts/check_breaking_changes.sh
+++ .github/workflows/scripts/check_breaking_changes.sh
@@ -3,11 +3,11 @@ PR_NUMBER=$1
 BASE_BRANCH=$2
 
 # Check for any Commit with the word "BREAKING CHANGE" in the commit message
-git fetch origin +refs/pull/${{ PR_NUMBER }}/merge
+git fetch origin +refs/pull/$PR_NUMBER/merge:
 git log --pretty=format:"%s" origin/main..FETCH_HEAD | grep -i "BREAKING CHANGE" > breaking_changes.txt
 
 # If there are breaking changes, and the base branch is not development, fail the build
-if [ -s breaking_changes.txt ] and [ ${{ BASE_BRANCH }} != "development" ]; then
+if [ -s breaking_changes.txt ] and [ $BASE_BRANCH != "development" ]; then
     echo "Breaking Changes Detected on a non-development branch. Please merge into development first."
     exit 1
 else
