234a3bea563612fc62656e2a4e494f081cf0f6b1
Xanth-64 <bet.and.xanth@gmail.com>
2024-02-05 16:29:09 -0400

    fix: Made it so that the breaking changes check looks at all of the commit message

--- .github/workflows/scripts/check_breaking_changes.sh
+++ .github/workflows/scripts/check_breaking_changes.sh
@@ -4,7 +4,7 @@ BASE_BRANCH=$2
 
 # Check for any Commit with the word "BREAKING CHANGE" in the commit message
 git fetch origin +refs/pull/$PR_NUMBER/merge:
-git log --pretty=format:"%s" origin/main..FETCH_HEAD | grep -i "BREAKING CHANGE" > breaking_changes.txt
+git log --pretty=format:"%B" origin/main..FETCH_HEAD | grep -i "BREAKING CHANGE" > breaking_changes.txt
 
 # If there are breaking changes, and the base branch is not development, fail the build
 if [ -s breaking_changes.txt ] and [ $BASE_BRANCH != "development" ]; then
